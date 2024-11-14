b1ab1130384a0c31f9c5dddb8992d900f4b41f27
Leander_W <wlc@zju.edu.cn>
2024-11-14 13:26:39 +0800

    for x86 PCIE

--- scripts/init_env.sh
+++ scripts/init_env.sh
@@ -24,4 +24,19 @@ mkdir -p /data/tmpdir
 source hub_venv/bin/activate
 export TMPDIR=/data/tmpdir # to avoid "no space left" error during pip package installation
 pip install --upgrade pip
-pip3 install -r requirements.txt --cache-dir /data/tmpdir
+pip3 install -r requirements.txt --cache-dir /data/tmpdir --extra-index-url https://download.pytorch.org/whl/cpu
+
+# 检查系统架构并安装相应的包
+ARCH=$(uname -m)
+
+if [ "$ARCH" == "x86_64" ]; then
+    pip3 install https://modelscope.cn/models/wlc952/aigchub_models/resolve/master/tpu_perf-1.2.60-py3-none-manylinux2014_x86_64.whl
+    echo "您需要自行编译安装sophon.sail"
+elif [ "$ARCH" == "aarch64" ]; then
+    pip3 install https://modelscope.cn/models/wlc952/aigchub_models/resolve/master/tpu_perf-1.2.60-py3-none-manylinux2014_aarch64.whl
+    pip3 install https://modelscope.cn/models/wlc952/aigchub_models/resolve/master/sophon_arm-3.8.0-py3-none-any.whl
+else
+    echo "不支持的系统架构: $ARCH"
+    exit 1
+fi
+
